9dae9fe9d4c0d04836630db451678f21deea97cf
lukasberns <lukas_berns15@yahoo.co.jp>
2012-07-06 07:11:16 +0900

    Render one point speaker and one line speaker

--- js/main.js
+++ js/main.js
@@ -1,12 +1,18 @@
 function main() {
+	var point1 = new PointSpeaker(100, 100, 50);
+	var line1 = new LineSpeaker(500, 100, 50);
+	
 	var renderer = new ScalarFieldRenderer;
 	renderer.fieldFunction = function(x, y) {
-		return Math.pow(Math.sin((x*x + y*y)/100), 2);
+		return soundPressureToSPL(
+			point1.evaluate(x,y)
+				+ line1.evaluate(x,y)
+		);
 	};
 	renderer.canvas = document.createElement('canvas');
 	renderer.canvas.width = 640;
 	renderer.canvas.height = 480;
-	renderer.render();
+	renderer.render(50);
 	
 	document.body.appendChild(renderer.canvas);
 }
